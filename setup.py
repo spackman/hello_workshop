464985a785d69919eae7cec028e9b59c7a204682
Isaac Spackman <87101526+spackman@users.noreply.github.com>
2024-06-29 14:01:31 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='hello_workshop',
+      version='1.0',
+      description='example package for workshop',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
