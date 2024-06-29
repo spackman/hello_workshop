b688b983b6351d359eb4420274ea75c944f5defd
Isaac Spackman <87101526+spackman@users.noreply.github.com>
2024-06-29 14:02:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,1 @@
-from distutils.core import setup
-
-setup(name='hello_workshop',
-      version='1.0',
-      description='example package for workshop',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-     )
+print("HELLO WORKSHOP")
